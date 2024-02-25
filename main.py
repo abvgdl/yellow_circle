88fbce18f4fddedaead3dd90bd1aa3d8d7ed08d9
abvgdl <senior.abdulmanoff@yandex.ru>
2024-02-25 23:03:53 +0500

    0.2

--- main.py
+++ main.py
@@ -3,24 +3,43 @@ import random
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.uic import loadUi
+from PyQt5 import QtCore, QtWidgets
 
 
-class CircleDrawer(QWidget):
+class Ui_main_window(object):
+    def setupUi(self, main_window):
+        main_window.setObjectName("main_window")
+        main_window.resize(560, 480)
+        self.button_draw = QtWidgets.QPushButton(main_window)
+        self.button_draw.setGeometry(QtCore.QRect(240, 230, 75, 23))
+        self.button_draw.setObjectName("button_draw")
+
+        self.retranslateUi(main_window)
+        QtCore.QMetaObject.connectSlotsByName(main_window)
+
+    def retranslateUi(self, main_window):
+        _translate = QtCore.QCoreApplication.translate
+        main_window.setWindowTitle(_translate("main_window", "yellow_circles"))
+        self.button_draw.setText(_translate("main_window", "PushButton"))
+
+
+class CircleDrawer(QWidget, Ui_main_window):
     def __init__(self):
         super().__init__()
-        loadUi('UI.ui', self)
+        super().setupUi(self)
         self.setWindowTitle('Генератор окружностей')
 
         self.button_draw.clicked.connect(self.draw_circle)
         self.diameter = 0
+        self.color = QColor('white')
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        color = QColor('yellow')
-        painter.setBrush(color)
+        painter.setBrush(self.color)
         painter.drawEllipse(50, 50, self.diameter, self.diameter)
 
     def draw_circle(self):
+        self.color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.diameter = random.randint(20, 100)
         self.update()
 
